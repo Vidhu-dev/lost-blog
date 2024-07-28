88191b6b7bbbb35bfdf85923bc8c7391289d22fd
Vidhu Prakash <vidhuprakash497@gmail.com>
2024-07-28 22:46:49 +0530

    removed  unnecessary file

--- clientv1/src/utils/api.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const API = axios.create({
-  baseURL: "http://localhost:8080/api/v1",
-});
-
-export const signup = (formData) => API.post("/users/register", formData);
-
-// export const login = (formData) => API.post("/users/login", formData);
--- clientv1/src/utils/getFile.js
+++
@@ -1,8 +0,0 @@
-export async function getFile({ filePath, fileName, fileFormat }) {
-  const response = await fetch(filePath);
-  const blob = await response.blob();
-  const file = new File([blob], fileName, {
-    type: fileFormat,
-  });
-  return file;
-}
